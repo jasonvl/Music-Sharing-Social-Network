bdf677f59117aa272e8970eafcfdb0d9ef690b1b
Jason Vazquez-Li <vazquezlijason@gmail.com>
2018-03-22 16:09:03 -0400

    Update upload.php

--- AWS_S3/upload.php
+++ AWS_S3/upload.php
@@ -20,8 +20,8 @@
 	use Aws\S3\Exception\S3Exception;
 	// AWS Info
 	$bucketName = 'music-network';
-	$IAM_KEY = 'AKIAIAB5NXEYKV7NP5JA';
-	$IAM_SECRET = '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';
+	$IAM_KEY = '';
+	$IAM_SECRET = '';
 	// Connect to AWS
 	try {
 		// You may need to change the region. It will say in the URL when the bucket is open
